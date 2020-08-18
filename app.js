5e9f62c86a4f0b847a71dcb633ea8c2153fe97fd
sree28 <mathi.sreemathi.sree@gmail.com>
2020-08-18 15:00:32 +0530

    third commit

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use((req,res,next)=>{
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eo3fa.gcp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
 {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true})
 .then(()=>{
-  app.listen(5000);
+  app.listen(process.env.PORT || 5000);
 }
 ).catch(err =>{
   console.log(err);
